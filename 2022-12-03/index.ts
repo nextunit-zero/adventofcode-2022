6372c91aa11a9773696caddecb6b7d2c8c8c37fb
Nils B <zero+github@nextunit.io>
2022-12-04 12:38:15 +0100

    Add secomd part of 3rd door

--- 2022-12-03/index.ts
+++ 2022-12-03/index.ts
@@ -1,39 +1,47 @@
 import * as fs from 'fs';
 import { join } from 'path';
 
+function getDuplicates(a: string, b: string): string[] {
+    const dupliactes = [];
+    a.split('').filter((char) => {
+        if (b.indexOf(char) !== -1 && !dupliactes.includes(char)) {
+            dupliactes.push(char);
+        }
+    })
+
+    return dupliactes;
+}
+
+function getProiority(char: string) {
+    const code = char.charCodeAt(0);
+    // Upper case letters in Ascii
+    if (code >= 65 && code <= 90) {
+        // First remove the Ascii overhead and bring A -> 0, them add the 27 for priority
+        return code - 65 + 27;
+    }
+    // Lower case letters in Ascii
+    if (code >= 97 && code <= 122) {
+        // First remove the Ascii overhead and bring a -> 0, them add the 1 for priority
+        return code - 97 + 1;
+    }
+}
+
 class Rucksack {
     constructor(public compartment1: string, public compartment2: string) { }
 
     public getDuplicates(): string[] {
-        const dupliactes = [];
-        this.compartment1.split('').filter((char) => {
-            if (this.compartment2.indexOf(char) !== -1 && !dupliactes.includes(char)) {
-                dupliactes.push(char);
-            }
-        })
-
-        return dupliactes;
+        return getDuplicates(this.compartment1, this.compartment2);
     }
 
     public getPrioirotyMap(): { [key: string]: number } {
         const priorities = {}
-        this.getDuplicates().forEach((char) => (priorities[char] = Rucksack.getProiority(char)));
+        this.getDuplicates().forEach((char) => (priorities[char] = getProiority(char)));
 
         return priorities;
     }
 
-    public static getProiority(char: string) {
-        const code = char.charCodeAt(0);
-        // Upper case letters in Ascii
-        if (code >= 65 && code <= 90) {
-            // First remove the Ascii overhead and bring A -> 0, them add the 27 for priority
-            return code - 65 + 27;
-        }
-        // Lower case letters in Ascii
-        if (code >= 97 && code <= 122) {
-            // First remove the Ascii overhead and bring a -> 0, them add the 1 for priority
-            return code - 97 + 1;
-        }
+    public getItems(): string {
+        return this.compartment1 + this.compartment2;
     }
 
     public static packRucksack(input: string): Rucksack {
@@ -44,10 +52,46 @@ class Rucksack {
     }
 }
 
+class Group {
+    private elfs: Rucksack[] = [];
+
+    public add(elf: Rucksack) {
+        if (this.elfs.length >= 3) {
+            throw new Error('Group already full');
+        }
+
+        this.elfs.push(elf);
+    }
+
+    public getBadges(): string[] {
+        if (this.elfs.length < 2) {
+            throw new Error('Too less elfs to compare')
+        }
+        let duplicates = this.elfs[0].getItems().split('');
+
+        this.elfs.forEach((elf) => {
+            duplicates = getDuplicates(duplicates.join(''), elf.getItems());
+        });
+
+        return duplicates;
+    }
+
+    public getBadge(): string {
+        const badges = this.getBadges();
+        if (badges.length !== 1) {
+            throw new Error('There are multiple badges available')
+        }
+
+        return badges[0];
+    }
+}
+
 const fileContent = fs.readFileSync(join(__dirname, './input.txt')).toString();
 const fileContentSplitByLines = fileContent.split("\n");
 
 const rucksacks = fileContentSplitByLines.map(Rucksack.packRucksack)
+
+const groups = [];
 
 let sum = 0;
 rucksacks.forEach((r, i) => {
@@ -55,6 +99,17 @@ rucksacks.forEach((r, i) => {
     console.log(`${i}: ${r.compartment1} || ${r.compartment2} ==> ${JSON.stringify(map)}`)
 
     Object.keys(map).forEach(k => sum += map[k]);
+
+    if (i % 3 === 0) {
+        groups.push(new Group());
+    }
+
+    groups[groups.length - 1].add(r);
 })
 
 console.log('Answer 1: ' + sum);
+
+let sumBadges = 0;
+groups.forEach((g) => sumBadges += (getProiority(g.getBadge())))
+
+console.log('Answer 2: ' + sumBadges);
