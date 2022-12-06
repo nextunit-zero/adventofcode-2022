f33b2ee74cc58e535019d6e87a381ab43d5f9fca
Nils B <zero+github@nextunit.io>
2022-12-06 09:05:14 +0100

    Add solution for part 2 of 6th door

--- 2022-12-06/index.ts
+++ 2022-12-06/index.ts
@@ -1,8 +1,12 @@
 import * as fs from 'fs';
 import { join } from 'path';
 
-function getPositionOfFirstUniqueString(input: string, markerPosition = 4) {
-    const substr = input.substring(markerPosition - 4, markerPosition).split('');
+function getPositionOfFirstUniqueString(input: string, uniqueStringSize: number, markerPosition = -1) {
+    if (markerPosition === -1) {
+        markerPosition = uniqueStringSize;
+    }
+
+    const substr = input.substring(markerPosition - uniqueStringSize, markerPosition).split('');
     const filteredDuplicates = substr.filter((item, index) => substr.indexOf(item) === index);
     if (filteredDuplicates.length === substr.length) {
         return markerPosition;
@@ -13,8 +17,9 @@ function getPositionOfFirstUniqueString(input: string, markerPosition = 4) {
         throw new Error('No position found');
     }
 
-    return getPositionOfFirstUniqueString(input, markerPosition);
+    return getPositionOfFirstUniqueString(input, uniqueStringSize, markerPosition);
 }
 
 const fileContent = fs.readFileSync(join(__dirname, './input.txt')).toString();
-console.log('Answer 1:', getPositionOfFirstUniqueString(fileContent));
+console.log('Answer 1:', getPositionOfFirstUniqueString(fileContent, 4));
+console.log('Answer 2:', getPositionOfFirstUniqueString(fileContent, 14));
